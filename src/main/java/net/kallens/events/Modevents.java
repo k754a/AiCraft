05e774fed40f39592e0d3efd6750a5993e97886b
k754a <kallenms27@g.eips.ca>
2025-03-05 16:53:13 -0700

    it should all work

--- src/main/java/net/kallens/events/Modevents.java
+++ src/main/java/net/kallens/events/Modevents.java
@@ -3,6 +3,7 @@ package net.kallens.events;
 import net.kallens.Command.SummonAI;
 import net.kallens.aiminecraft.AiMinecraft;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.server.command.ConfigCommand;
@@ -11,10 +12,20 @@ import net.minecraftforge.server.command.ConfigCommand;
 public class Modevents {
 
     @SubscribeEvent
-    public  static  void onCommandsRegister(RegisterCommandsEvent event){
-         new SummonAI(event.getDispatcher());
+    public static void onCommandsRegister(RegisterCommandsEvent event) {
+
+        new SummonAI(event.getDispatcher());
 
 
         ConfigCommand.register(event.getDispatcher());
     }
+
+//    @SubscribeEvent
+//    public static void onPlayerCloneEvent(PlayerEvent.Clone event) {
+//
+//        if (!event.getOriginal().getLevel().isClientSide()) {
+//
+//            event.getPlayer().getPersistentData().putIntArray(AiMinecraft.MODID +"aipos",event.getOriginal().getPersistentData().getIntArray(AiMinecraft.MODID +"aipos"));
+//        }
+//    }
 }
